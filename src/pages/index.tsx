5d2dccd4becaee529e9dd39d9ab9d9a302f7bc7f
mina <m1nna@naver.com>
2022-01-26 18:25:08 +0900

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,23 @@
 import React, { FunctionComponent } from 'react'
-import Text from 'components/Text'
+import styled from '@emotion/styled'
+import GlobalStyle from 'components/Common/GlobalStyle'
+import Introduction from 'components/Main/Introduction'
+import Footer from 'components/Common/Footer'
+
+const Container =styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`
 
 const IndexPage: FunctionComponent = () => {
-  return <Text text="Home" />
-}
+  return (
+  <Container>
+    <GlobalStyle />
+    <Introduction />
+    <Footer />
+  </Container>
+  )}
 
 export default IndexPage
+
